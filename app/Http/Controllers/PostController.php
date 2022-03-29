44d5b6ca3e7c90ff6273159625d0b4c9402cf124
PeterN00 <nguyentanphuc03092000hiie@gmail.com>
2022-03-29 19:51:05 +0700

    Job details

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
 class PostController extends Controller
 {
@@ -24,5 +27,15 @@ class PostController extends Controller
             $posts=$posts->where('level_id',$request->level);
         $posts=$posts->paginate(15);
         return view('search')->with('posts',$posts);
+    }
+    public function show($id)
+    {
+        //
+        $post=Post::where('id',$id)->get()->first();
+        $description=Storage::disk('local')->get($post->description);
+        $requirement=Storage::disk('local')->get($post->requirement);
+        return view('job')->with(['post'=>$post,
+            'description'=>str_replace("\n",'<br>',htmlentities($requirement)),
+            'requirement'=>str_replace("\n",'<br>',htmlentities($requirement)),]);
     }
 }
